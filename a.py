bfc977f72c664e541c5be999d8e87ca6d8d79d8b
hanato238 <les.ensoleilles@gmail.com>
2025-03-23 00:02:56 +0900

    commit

--- a.py
+++ a.py
@@ -2,9 +2,14 @@ import sys
 sys.path.append("C:\\Users\\lesen\\workspace\\clinic")
 
 
-from data_utils import data_loader, get_aga_rate, get_freq
+from data_utils import data_loader, get_aga_rate, get_freq, get_expected_profit
 
 file_path = 'data.xlsx'
 data = data_loader(file_path)
+freq = get_freq(data)
+aga = get_aga_rate(data)
+datum = data['Q8S1_薄毛対策へかけている金額_薄毛_男性']
+profit = get_expected_profit(data, freq, aga, datum)
+
 for name in data:
     print(name)
